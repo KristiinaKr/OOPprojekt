b541a82d4642ea1d701ba701f05fe3e0844921a3
KristiinaKr <krausekristiina@gmail.com>
2020-03-17 11:29:20 +0200

    Esimese ülesande kasutajaliides ja random numbrite genereerimine sai kenaks.

---
+++ testKlass.java
@@ -0,0 +1,29 @@
+package OOPprojekt;
+
+import javax.swing.*;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class testKlass {
+
+    public static void main(String[] args) {
+
+        double katkineEsimesest = Math.round(ThreadLocalRandom.current().nextDouble(0.6,0.9)*10)/10.0;
+        double katkineTeisest = Math.round(ThreadLocalRandom.current().nextDouble(0.1,0.5)*10)/10.0;
+        double katkineKolmandast = Math.round(ThreadLocalRandom.current().nextDouble(0.1,0.5)*10)/10.0;
+
+       //ÜL1 ülesanne1 = new ÜL1 (katkineEsimesest, katkineTeisest, katkineKolmandast);
+
+        // Nende muutujatega testimiseks on õige vastus 0.5
+        ÜL1 ülesanne1 = new ÜL1 (0.6, 0.3, 0.3);
+        ülesanne1.lahendus();
+
+
+
+        ÜL2 ülesanne2 = new ÜL2((int) Math.round(Math.random()*10)+1,
+                (int) Math.round(Math.random()*10)+1,(int) Math.round(Math.random()*10)+1);
+
+        //ülesanne2.lahendus2();
+        System.out.println(ülesanne2.lahendus2());
+    }
+}
--- ÜL1.java
+++ ÜL1.java
@@ -45,23 +45,35 @@ public class ÜL1 {
         this.katkineKolmandast = katkineKolmandast;
     }
 
+
     public double lahendus() {
 
-        double lahendus = Math.round(katkineEsimesest*katkineTeisest*katkineKolmandast*100)/100.0;
-        String kasutajaLahendus;
-        kasutajaLahendus = JOptionPane.showInputDialog(null,
-                        "Toidupoes sai valida piimakoti kolmest sarnasest kastist. On teada,\n" +
-                        " et ühes neist kastidest on katkise piimakoti saamise tõenäolsus palju suurem,\n" +
-                        " kui ülejäänud kahes kastis. Ostja saigi katkise piimakoti.\n" +
-                        " Milline on tõenäolsus, et ta sai selle 'halvast' kastist? \n Sisesta vastus: ", "Ülesanne",
-                JOptionPane.QUESTION_MESSAGE);
-        JOptionPane.showMessageDialog(null, lahendus);
+        double lahendus = Math.round(((katkineEsimesest * 1 / 3) / (katkineEsimesest * 1 / 3 + katkineTeisest * 1 / 3 + katkineKolmandast * 1 / 3)) * 100) / 100.0;
+        String kasutajalahendus = "0";
+        while (Double.parseDouble(kasutajalahendus) != lahendus) {
+            kasutajalahendus = JOptionPane.showInputDialog(null,
+                    "Toidupoes sai valida piimakoti kolmest sarnasest kastist. On teada,\n" +
+                            " et ühes neist kastidest on katkise piimakoti saamise tõenäolsus " + katkineEsimesest + "\n" +
+                            " ning ülejäänud kastides vaid  " + katkineTeisest + " ja " + katkineKolmandast + ". Ostja saigi katkise piimakoti.\n" +
+                            " Milline on tõenäolsus, et ta sai selle 'halvast' kastist? \n Sisesta vastus: ", "Ülesanne 1",
+                    JOptionPane.QUESTION_MESSAGE);
+
+            if (Double.parseDouble(kasutajalahendus) == lahendus) {
+                JOptionPane.showMessageDialog(null, "Tubli! " + "\n" + " Sinu vastus on õige!");
+                break;
+            } else {
+                JOptionPane.showMessageDialog(null,
+                        "Sinu vastus ei ole õige! \n" + "Vihje: kasuta Bayes'i valemit!",
+                        "Vale vastus",
+                        JOptionPane.WARNING_MESSAGE);
+                String valik = JOptionPane.showInputDialog(null,
+                        "Kas soovid ülesannet uuesti proovida (jah/ei)?", JOptionPane.QUESTION_MESSAGE);
+                if (valik.equals("ei")) break;
+            }
+        }
+        JOptionPane.showMessageDialog(null, "Õige vastus on: " + lahendus);
         System.exit(0);
         return lahendus;
-
     }
-
-
-
 }
 
