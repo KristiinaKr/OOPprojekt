f3651a5911832e77403bf478c1afd5e3aa88573a
KristiinaKr <krausekristiina@gmail.com>
2020-04-25 13:07:23 +0300

    Graafilise kasutajaliidese õige vastuse sisestamise haru iga ülesande puhul valmis + klassidevaheline liikumine testklassist kasutajaliidesesse (isendite ja Stage'ide loomine)

--- Kasutajaliides.java
+++ Kasutajaliides.java
@@ -1,94 +1,248 @@
 package OOPprojekt;
 
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.*;
+import javafx.stage.Stage;
+import org.w3c.dom.ls.LSOutput;
+
+import javax.crypto.spec.PSource;
 import javax.swing.*;
+import java.text.NumberFormat;
 import java.util.concurrent.ThreadLocalRandom;
 
-public class Kasutajaliides {
+public class Kasutajaliides extends Application {
 
+    //private String ülesandepüstitus;
+    Ülesanne lahendatavÜlesanne = null;
 
-    private String ülesandepüstitus;
+    private String ülesandeValik;
 
+    /*KONSTRUKTOR vana
     public Kasutajaliides(String ülesandepüstitus) {
         this.ülesandepüstitus = ülesandepüstitus;
+    } */
+
+    public Kasutajaliides(String ülesandeValik) {
+        this.ülesandeValik = ülesandeValik;
     }
 
-    public void kasutajaLiides(String ülesandeValik) {
+    public String getLahendatavÜlesanne() {
+        return ülesandeValik;
+    }
 
-        Ülesanne lahendatavÜlesanne = null;
+    public void start(Stage ülesanne) throws Exception {
 
-        if (ülesandeValik.equals("2")) {
-            String pealkiri2 = "Ülesanne 2";
-            String vihje2 = "Vihje: Tinglik tõenäosus!";
-            int esimeneArv = (int) (Math.random()* 1) + 2;
-            int teineArv = (int) (Math.random()* 2) + 4;
-            int kolmasArv = (int) (Math.random()* 3) + 6;
+        BorderPane piiriPaan = new BorderPane();
+        piiriPaan.setPadding(new Insets(30, 20, 15, 20));
+        piiriPaan.setStyle("-fx-background-color: #FBE4F1;");
 
-            String ülesandepüstitus2 = "Juhuslikult valitakse arv ühest tuhandeni(kaasa arvatud). Leia\n" +
-                    "tõenäosus, et see jagub vähemalt ühega arvudest " + esimeneArv +
-                    ", " + teineArv + " ja " + kolmasArv + ".\n";
-
-            lahendatavÜlesanne = new ÜL2(esimeneArv, teineArv, kolmasArv, pealkiri2, ülesandepüstitus2, vihje2);
-        } else if (ülesandeValik.equals("1")) {
-            String pealkiri1 = "Ülesanne 1";
-            String vihje1 = "Vihje: Kasuta Bayes'i valemit!";
-            double katkineEsimesest = Math.round(ThreadLocalRandom.current().nextDouble(0.6, 0.9) * 10) / 10.0;
-            double katkineTeisest = Math.round(ThreadLocalRandom.current().nextDouble(0.1, 0.5) * 10) / 10.0;
-            double katkineKolmandast = Math.round(ThreadLocalRandom.current().nextDouble(0.1, 0.5) * 10) / 10.0;
-
-            String ülesandePüstitus1 = "Toidupoes sai valida piimakoti kolmest sarnasest kastist. On teada,\n" + "et ühes neist kastidest on " +
-                    "katkise piimakoti saamise tõenäolsus " + katkineEsimesest + "\n" + "ning ülejäänud kastides vaid  " + katkineTeisest + " ja "
-                    + katkineKolmandast + ". Ostja saigi katkise piimakoti.\n" + "Milline on tõenäolsus, et ta sai selle 'halvast' kastist? \n";
-
-            lahendatavÜlesanne = new ÜL1(katkineEsimesest, katkineTeisest, katkineKolmandast, pealkiri1, ülesandePüstitus1, vihje1);
+        VBox vbox = new VBox();
+        vbox.setAlignment(Pos.BOTTOM_CENTER);
+        vbox.setSpacing(10);
 
 
-        } else if (ülesandeValik.equals("3")) {
-            String pealkiri3 = "Ülesanne 3";
-            String vihje3 = "Kasuta Bernoulli valemit eraldi mõlema poisi kõigi tabamuste võimaluste korral.";
-            int õunteArv = (int) Math.round(ThreadLocalRandom.current().nextDouble(0.2, 0.6) * 10);
-            double jukuTäpsus = Math.round(ThreadLocalRandom.current().nextDouble(0.1, 0.9) * 10) / 10.0;
-            double mikuTäpsus = Math.round(ThreadLocalRandom.current().nextDouble(0.1, 0.9) * 10) / 10.0;
+        TextField sisestus2 = new TextField("Sisesta vastus");
+        sisestus2.setMaxWidth(105);
 
-            String ülesandePüstitus3 = "Jukul ja Mikul on mõlemal " + õunteArv + " õuna. Peale õunte söömist viskavad nad õunasüdametega\n" +
-                    "paberikorvi täpsust. Juku tabab tõenäolsusega " + jukuTäpsus + " ning Miku tabab tõenäolsusega \n" + mikuTäpsus +
-                    ". Leia tõenäolsus, et nad saavad võrdse arvu tabamusi.\n";
-
-            lahendatavÜlesanne = new ÜL3(õunteArv, jukuTäpsus, mikuTäpsus, pealkiri3, ülesandePüstitus3, vihje3);
-
-        } else if (ülesandeValik.equals("4")){
-            String pealkiri4 = "Ülesanne 4";
-            String vihje4 = "Keskväärtuse leidmiseks ei pea tingimata jaotust leidma!";
-            int esimeneValge = (int) (Math.random()* 7) + 2;
-            int esimeneMust = (int) (Math.random()* 7) + 2;
-            int teineValge = (int) (Math.random()* 7) + 2;
-            int teineMust = (int) (Math.random()* 7) + 2;
-
-            String ülesndePüstitus4 = "Laual on kaks urni, milles ühes on " + esimeneValge + " valget ja " +
-                   esimeneMust + " musta kuuli ning \nteises on "+ teineValge + " valget ja " + teineMust +
-                    " musta kuuli. Kummastki urnist valitakse kaks \nkuuli ning X " +
-                    "on saadud valgete kuulide koguarv. Leida X keskväärtus.\n";
-
-            lahendatavÜlesanne = new ÜL4(esimeneValge, esimeneMust, teineValge, teineMust, pealkiri4, ülesndePüstitus4, vihje4);
-        } else if (ülesandeValik.equals("5")) {
-            String pealkiri5 = "Ülesanne 5 ";
-            String vihje5 = "Kui katsete arv on > 100, võib olla mõistlik lahendada Binoomjaotuse asemel Poissoni jaotusega!";
+        Button nupp2 = new Button("Kinnita");
+        vbox.getChildren().addAll(sisestus2, nupp2);
+        piiriPaan.setBottom(vbox);
 
 
-            int sülearvutiteArv = (int) Math.round(ThreadLocalRandom.current().nextDouble(10, 95) * 100);
-            double rikkiMinemiseTõenäolsus = Math.round(ThreadLocalRandom.current().nextDouble(0.2, 0.9) * 10)/10.0;
-            int vähemaltMituRikkist = (int) Math.round(ThreadLocalRandom.current().nextDouble(0.1, 0.6) * 10);
+        switch (ülesandeValik) {
+            case "1": {
+                String pealkiri1 = "Ülesanne 1";
+                String vihje1 = "Vihje: Kasuta Bayes'i valemit!";
+                double katkineEsimesest = Math.round(ThreadLocalRandom.current().nextDouble(0.6, 0.9) * 10) / 10.0;
+                double katkineTeisest = Math.round(ThreadLocalRandom.current().nextDouble(0.1, 0.5) * 10) / 10.0;
+                double katkineKolmandast = Math.round(ThreadLocalRandom.current().nextDouble(0.1, 0.5) * 10) / 10.0;
+
+                String ülesandePüstitus1 = "Toidupoes sai valida piimakoti kolmest sarnasest kastist. On teada,\n" + "et ühes neist kastidest on " +
+                        "katkise piimakoti saamise tõenäolsus " + katkineEsimesest + "\n" + "ning ülejäänud kastides vaid  " + katkineTeisest + " ja "
+                        + katkineKolmandast + ". Ostja saigi katkise piimakoti.\n" + "Milline on tõenäolsus, et ta sai selle 'halvast' kastist? \n";
+
+                lahendatavÜlesanne = new ÜL1(katkineEsimesest, katkineTeisest, katkineKolmandast, pealkiri1, ülesandePüstitus1, vihje1);
+
+                Text tekst = new Text(ülesandePüstitus1);
+                tekst.setFont(Font.font("verdana", FontWeight.SEMI_BOLD, FontPosture.REGULAR, 13));
+                tekst.setTextAlignment(TextAlignment.CENTER);
+                piiriPaan.setCenter(tekst);
+                break;
+            }
+
+            case "2": {
+                String pealkiri2 = "Ülesanne 2";
+                String vihje2 = "Vihje: Tinglik tõenäosus!";
+                int esimeneArv = (int) (Math.random() * 1) + 2;
+                int teineArv = (int) (Math.random() * 2) + 4;
+                int kolmasArv = (int) (Math.random() * 3) + 6;
+
+                String ülesandePüstitus2 = "Juhuslikult valitakse arv ühest tuhandeni(kaasa arvatud). \n " +
+                        "Leia tõenäosus, et see jagub vähemalt ühega \n arvudest " + esimeneArv +
+                        ", " + teineArv + " ja " + kolmasArv + ".\n";
+
+                lahendatavÜlesanne = new ÜL2(esimeneArv, teineArv, kolmasArv, pealkiri2, ülesandePüstitus2, vihje2);
+
+                Text tekst = new Text(ülesandePüstitus2);
+                tekst.setFont(Font.font("verdana", FontWeight.SEMI_BOLD, FontPosture.REGULAR, 13));
+                tekst.setTextAlignment(TextAlignment.CENTER);
+                piiriPaan.setCenter(tekst);
+                break;
+            }
+
+            case "3": {
+                String pealkiri3 = "Ülesanne 3";
+                String vihje3 = "Kasuta Bernoulli valemit eraldi mõlema poisi kõigi tabamuste võimaluste korral.";
+                int õunteArv = (int) Math.round(ThreadLocalRandom.current().nextDouble(0.2, 0.6) * 10);
+                double jukuTäpsus = Math.round(ThreadLocalRandom.current().nextDouble(0.1, 0.9) * 10) / 10.0;
+                double mikuTäpsus = Math.round(ThreadLocalRandom.current().nextDouble(0.1, 0.9) * 10) / 10.0;
+
+                String ülesandePüstitus3 = "Jukul ja Mikul on mõlemal " + õunteArv + " õuna. Peale õunte söömist \n viskavad nad õunasüdametega " +
+                        "paberikorvi täpsust. \n Juku tabab tõenäolsusega " + jukuTäpsus + " ning Miku tabab  \n tõenäolsusega " + mikuTäpsus +
+                        ". Leia tõenäolsus, et nad saavad \n võrdse arvu tabamusi.\n";
+
+                lahendatavÜlesanne = new ÜL3(õunteArv, jukuTäpsus, mikuTäpsus, pealkiri3, ülesandePüstitus3, vihje3);
+
+                Text tekst = new Text(ülesandePüstitus3);
+                tekst.setFont(Font.font("verdana", FontWeight.SEMI_BOLD, FontPosture.REGULAR, 13));
+                tekst.setTextAlignment(TextAlignment.CENTER);
+                piiriPaan.setCenter(tekst);
+                break;
+            }
+
+            case "4": {
+                String pealkiri4 = "Ülesanne 4";
+                String vihje4 = "Keskväärtuse leidmiseks ei pea tingimata jaotust leidma!";
+                int esimeneValge = (int) (Math.random() * 7) + 2;
+                int esimeneMust = (int) (Math.random() * 7) + 2;
+                int teineValge = (int) (Math.random() * 7) + 2;
+                int teineMust = (int) (Math.random() * 7) + 2;
+
+                String ülesndePüstitus4 = "Laual on kaks urni, milles ühes on " + esimeneValge + " valget ja " +
+                        esimeneMust + " musta kuuli ning \nteises on " + teineValge + " valget ja " + teineMust +
+                        " musta kuuli. Kummastki urnist valitakse kaks \nkuuli ning X " +
+                        "on saadud valgete kuulide koguarv. Leida X keskväärtus.\n";
+
+                lahendatavÜlesanne = new ÜL4(esimeneValge, esimeneMust, teineValge, teineMust, pealkiri4, ülesndePüstitus4, vihje4);
+
+                Text tekst = new Text(ülesndePüstitus4);
+                tekst.setFont(Font.font("verdana", FontWeight.SEMI_BOLD, FontPosture.REGULAR, 13));
+                tekst.setTextAlignment(TextAlignment.CENTER);
+                piiriPaan.setCenter(tekst);
+                break;
+            }
+
+            case "5": {
+                String pealkiri5 = "Ülesanne 5 ";
+                String vihje5 = "Kui katsete arv on > 100, võib olla mõistlik lahendada Binoomjaotuse asemel Poissoni jaotusega!";
 
 
+                int sülearvutiteArv = (int) Math.round(ThreadLocalRandom.current().nextDouble(10, 95) * 100);
+                double rikkiMinemiseTõenäolsus = Math.round(ThreadLocalRandom.current().nextDouble(0.2, 0.9) * 10) / 10.0;
+                int vähemaltMituRikkist = (int) Math.round(ThreadLocalRandom.current().nextDouble(0.1, 0.6) * 10);
 
-            String ülesandePüstitus5 = "Tehas saatis lattu " + sülearvutiteArv + " töökorras sülearvutit. \n" +
-                    "Tõenäolsus, et teel lattu läheb sülearvuti katki, on " + rikkiMinemiseTõenäolsus + ". \n" +
-                    "Leia tõenäolsus, et  lattu jõuab vähemalt " + vähemaltMituRikkist + " rikkist sülearvutit (ümarda 3 komakohta). \n";
 
-            lahendatavÜlesanne = new ÜL5(sülearvutiteArv, rikkiMinemiseTõenäolsus, vähemaltMituRikkist, pealkiri5,ülesandePüstitus5, vihje5);
+                String ülesandePüstitus5 = "Tehas saatis lattu " + sülearvutiteArv + " töökorras sülearvutit. \n" +
+                        "Tõenäolsus, et teel lattu läheb sülearvuti katki, on " + rikkiMinemiseTõenäolsus + ". \n" +
+                        "Leia tõenäolsus, et  lattu jõuab vähemalt " + vähemaltMituRikkist + " rikkist sülearvutit \n (ümarda 3 komakohta). \n";
 
+                lahendatavÜlesanne = new ÜL5(sülearvutiteArv, rikkiMinemiseTõenäolsus, vähemaltMituRikkist, pealkiri5, ülesandePüstitus5, vihje5);
+
+                Text tekst = new Text(ülesandePüstitus5);
+                tekst.setFont(Font.font("verdana", FontWeight.SEMI_BOLD, FontPosture.REGULAR, 13));
+                tekst.setTextAlignment(TextAlignment.CENTER);
+                piiriPaan.setCenter(tekst);
+                break;
+            }
         }
 
-        String kasutajalahendus = "0";
+
+
+        nupp2.setOnMouseClicked(event -> {
+
+            ülesanne.close();
+            //Siin praegu see variant, kui sisestati õige vastus, aint kuna ma ei arvuta igal testimisel
+            //õiget vastust välja siis panin järg reale != mitte ==
+                if (Double.parseDouble(sisestus2.getText()) != lahendatavÜlesanne.lahendus()) {
+                    Stage õige = new Stage();
+                    BorderPane bp = new BorderPane();
+                    bp.setPadding(new Insets(10, 20, 10, 20));
+                    bp.setStyle("-fx-background-color: #FBE4F1;");
+
+
+                    Text tekst2 = new Text("Tubli! Õige vastus! \n " +
+                            "Kas soovid veel ülesandeid lahendada?");
+                    tekst2.setTextAlignment(TextAlignment.CENTER);
+                    tekst2.setFont(Font.font("verdana", FontWeight.SEMI_BOLD, FontPosture.REGULAR, 13));
+
+
+                    Button jah = new Button("Jah");
+                    Button ei = new Button(" Ei ");
+                    HBox hBox = new HBox();
+                    hBox.setAlignment(Pos.BOTTOM_CENTER);
+                    hBox.setSpacing(25);
+                    hBox.setPadding(new Insets(10,10,20,10));
+                    hBox.getChildren().addAll(jah, ei);
+
+                    bp.setCenter(tekst2);
+                    bp.setBottom(hBox);
+
+
+                    jah.setOnMouseClicked(event2 -> {
+                        testKlass testklass = new testKlass();
+                        Stage pealava = new Stage();
+                        try {
+                            testklass.start(pealava);
+                            õige.close();
+                        } catch (Exception e) {
+                            System.out.println("Midagi juhtus");
+                        }
+                    });
+
+                    ei.setOnMouseClicked(event3 -> {
+                        õige.close();
+                        ülesanne.close();
+                    });
+
+                    Scene stseen2 = new Scene(bp, 300, 160);
+                    õige.setScene(stseen2);
+                    õige.show();
+
+                    /*Stage vale = new Stage();
+                    Text tekst2 = new Text("Vale vastus");
+                    vbox2.getChildren().add(tekst2);
+                    Scene stseen2 = new Scene(vbox2);
+                    vale.setScene(stseen2);
+                    vale.show();
+                    ülesanne.close();
+                     */
+                }
+        });
+
+        Scene stseen1 = new Scene(piiriPaan, 550, 200);
+        ülesanne.setTitle("Siin on sinu ülesanne");
+        ülesanne.setScene(stseen1);
+        ülesanne.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+
+    }
+
+
+
+
+    /*
+       // String kasutajalahendus = "0";
         assert lahendatavÜlesanne != null;
         while (Double.parseDouble(kasutajalahendus) != lahendatavÜlesanne.lahendus()) {
 
@@ -127,4 +281,7 @@ public class Kasutajaliides {
         }
         System.exit(0);
     }
+
+
+     */
 }
--- testKlass.java
+++ testKlass.java
@@ -3,61 +3,67 @@ package OOPprojekt;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-import javax.swing.*;
 
 public class testKlass extends Application {
 
     public void start(Stage peaLava) throws Exception {
 
         BorderPane piiriPaan = new BorderPane();
-        piiriPaan.setPadding(new Insets(30, 20, 15, 20));
+        piiriPaan.setPadding(new Insets(10, 20, 35, 20));
+        piiriPaan.setStyle("-fx-background-color: #FBE4F1;");
 
-        Text tekst = new Text("Mitmendat ülesannet soovid lahendada? (1-5)");
+        Text tekst = new Text("Mitmendat ülesannet soovid lahendada (1-5)?");
         tekst.setFont(Font.font("verdana", FontWeight.SEMI_BOLD, FontPosture.REGULAR, 12));
-        //tekst.setFill(Color.WHITE);
-        piiriPaan.setTop(tekst);
-
-        TextField sisestus = new TextField("");
-        sisestus.setMaxWidth(50);
-        piiriPaan.setCenter(sisestus);
-       piiriPaan.setStyle("-fx-background-color: #FBE4F1;");
-               //"#DD4184
-        String ülesandeValik = sisestus.getText();
+        piiriPaan.setCenter(tekst);
 
         VBox vbox = new VBox();
         vbox.setAlignment(Pos.BOTTOM_CENTER);
+        vbox.setSpacing(15);
+
+        TextField sisestus = new TextField("");
+        sisestus.setMaxWidth(50);
+        String ülesandeValik = sisestus.getText();
 
         Button nupp = new Button("Lahendama!");
-        //nupp.setOnMouseClicked(); siis lähme lahendama
-        vbox.getChildren().add(nupp);
+        vbox.getChildren().addAll(sisestus, nupp);
         piiriPaan.setBottom(vbox);
+
+        nupp.setOnMouseClicked(event -> {
+            if(!sisestus.getText().equals("")) {
+
+                //Kui sisestus ei ole nr 1-5, tuleks vale sisestuse exception catchida ja uuesti küsida
+                Kasutajaliides test = new Kasutajaliides(sisestus.getText());
+                Stage ülesanne = new Stage();
+                try {
+                    test.start(ülesanne);
+                    peaLava.close();
+                } catch (Exception e) {
+                    System.out.println("Kasutajaliidesesse liikumise viga");
+                }
+
+                peaLava.close();
+            }
+            //Kui midagi ei sisestatud, tuleks ka teha exception ja lasta uuesti sisestada
+            peaLava.close();
+        });
 
         Scene stseen1 = new Scene(piiriPaan, 310, 180);
         peaLava.setTitle("Alustame!");
         peaLava.setScene(stseen1);
         peaLava.show();
 
-        nupp.setOnMouseClicked(event -> {
-            System.out.println("Valisid ülesande " + sisestus.getText());
-            peaLava.close();
-        });
 
-        Kasutajaliides test = new Kasutajaliides(ülesandeValik);
-        System.out.println(ülesandeValik);
     }
 
     public static void main(String[] args) {
